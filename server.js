074933d166a3d234ff0743468738cf45023e4078
Shivkumar <shivkumarsalunkhe50gmail.com>
2022-12-16 12:52:02 +0530

    Added Cloudnary For Image Upload

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
 // connect mongoose
-mongoose.connect("mongodb+srv://shivkumar:shivkumarproject@cluster0.ukvzdar.mongodb.net/InstaCloneDB")
-
-
+// mongoose.connect("mongodb+srv://shivkumar:shivkumarproject@cluster0.ukvzdar.mongodb.net/InstaCloneDB")
+mongoose.connect('mongodb+srv://shivkumar:shivkumarproject@cluster0.ukvzdar.mongodb.net/InstaCloneDB', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+app.use(bodyParser.json())
+app.use(express.json());
 app.use(cors());
 app.use(express.json());
 
 app.use("/", require("./Routes/PostRoute"))
+
 
 app.get("*", (req,res)=>{
     res.status(500).send("FAILD API")
